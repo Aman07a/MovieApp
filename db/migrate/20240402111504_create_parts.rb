2822d523627760f3daeec77a22783ae49ed865f7
Aman07a <aman16.aa17@gmail.com>
2024-04-02 13:59:48 +0200

    Models II

--- db/migrate/20240402111504_create_parts.rb
+++ db/migrate/20240402111504_create_parts.rb
@@ -1,8 +1,8 @@
 class CreateParts < ActiveRecord::Migration[7.1]
   def change
     create_table :parts do |t|
-      t.references :movie, foreign_key: true
-      t.references :actor, foreign_key: true
+      t.belongs_to :movie, index: true
+      t.belongs_to :actor, index: true
       t.timestamps
     end
   end
